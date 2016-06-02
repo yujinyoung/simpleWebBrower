166de1f816d6fb7b20fb2294d61a4f6323459437
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-06-02 19:24:19 +0900

    #1 UI design

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/simplewebbrowser/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/simplewebbrowser/MainActivity.java
@@ -2,12 +2,33 @@ package kr.hs.emirim.dbwlsdud0407.simplewebbrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText editUrl;
+    Button butMove, butPrev;
+    WebView web;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editUrl=(EditText)(findViewById(R.id.edit_url));
+        butMove=(Button)findViewById(R.id.but_move);
+        butPrev=(Button)findViewById(R.id.but_prev);
+        web=(WebView)findViewById(R.id.web);
+        butMove.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+
     }
 }
