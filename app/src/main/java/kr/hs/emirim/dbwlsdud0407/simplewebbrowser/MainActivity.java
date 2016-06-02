830dd9c83ced77f5043ec7957660fd125ecaaf37
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-06-02 20:54:14 +0900

    #2 Event Programing

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/simplewebbrowser/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/simplewebbrowser/MainActivity.java
@@ -3,7 +3,9 @@ package kr.hs.emirim.dbwlsdud0407.simplewebbrowser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,6 +22,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         butPrev=(Button)findViewById(R.id.but_prev);
         web=(WebView)findViewById(R.id.web);
         butMove.setOnClickListener(this);
+        butPrev.setOnClickListener(this);
+        web.setWebViewClient(new WebViewClient());
+        WebSettings webset=web.getSettings();
+        webset.setBuiltInZoomControls(true);
+        webset.setJavaScriptEnabled(true);
+        web.loadUrl("http://www.naver.com");
     }
 
     /**
@@ -29,6 +37,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick(View v) {
+        String url=null;
+        switch(v.getId()){
+            case R.id.but_move:
+                url=editUrl.getText().toString();
+                if(!url.contains("http:"))
+                    url="http://www."+url;
+                web.loadUrl(url);
+                break;
+            case R.id.but_prev:
+                web.goBack();
+                break;
+        }
 
     }
 }
